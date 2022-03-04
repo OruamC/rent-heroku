fffb28299b0194d38bea906fc8a9846bd99ce36a
Oruam <oru_catani@hotmail.com>
2022-03-03 21:47:26 -0300

    Hello World

---
+++ src/main/java/com/oruam/rent/controllers/RentCotroller.java
@@ -0,0 +1,14 @@
+package com.oruam.rent.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RentCotroller {
+
+	public ResponseEntity<String> hello() {
+		return ResponseEntity.ok("Hello World");
+	}
+}
